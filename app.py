2c509a09ff783b6fbfd18126470ee53f9ac8ace3
Tom_XV <55909638+TomXV@users.noreply.github.com>
2022-02-05 12:59:26 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,67 @@
+from datetime import datetime
+
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30), nullable=False)
+    detail = db.Column(db.String(100))
+    due = db.Column(db.DateTime, nullable=False)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        posts = Post.query.all()
+        return render_template('index.html', posts=posts)
+    else:
+        title = request.form.get('title')
+        detail = request.form.get('detail')
+        due = request.form.get('due')
+
+        due = datetime.strptime(due, '%Y-%m-%d')
+        new_post = Post(title=title, detail=detail, due=due)
+
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect('/')
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+@app.route('/detail/<int:id>')
+def read(id):
+    post = Post.query.get(id)
+    return render_template('detail.html', post=post)
+
+@app.route('/update/<int:id>', methods = ['GET', 'POST'])
+def update(id):
+    post = Post.query.get(id)
+    if request.method == 'GET':
+        return render_template('update.html', post=post)
+    else:
+        post.title = request.form.get('title')
+        post.detail = request.form.get('detail')
+        post.due = datetime.strptime(request.form.get('due'), '%Y-%m-%d')
+
+        db.session.commit()
+        return redirect('/')
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    post = Post.query.get(id)
+
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/')
+
+
+if __name__=='__main__':
+    app.run(debug=True)
